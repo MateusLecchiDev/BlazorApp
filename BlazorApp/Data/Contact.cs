0a54e8beb0197da23e97bfde419d942debbd46b0
Mateus Lecchi <mateusrlecchi@gmail.com>
2026-01-06 16:25:19 -0300

    Corrigindo o Create, para que não seja possível criar contato em branco. Mudança no link de voltar para ser um botão de voltar.

--- BlazorApp/Data/Contact.cs
+++ BlazorApp/Data/Contact.cs
@@ -6,9 +6,11 @@ namespace BlazorApp.Data
     {
         public int Id { get; set; }
 
+        [Required(ErrorMessage = "O nome é obrigatório")]
         [MaxLength(100)]
         public string Name { get; set; } = string.Empty;
 
+        [Required(ErrorMessage = "O e-mail é obrigatório")]
         [MaxLength(200)]
         public string Email { get; set; } = string.Empty;
     }
